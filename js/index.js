88d85436f759c65df7bc23c5b3ad2254af0fe902
evansiroky <evan.siroky@yahoo.com>
2016-03-22 16:42:39 -0700

    Adding more details to day of event

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@ var $ = require('jquery'),
   analytics = require('ga-browser')(),
   tether = require('tether'),
   bootstrap = require('bootstrap'),
-  config = require('./config.js')
+  UAParser = require('ua-parser-js')
+
+var config = require('./config.js')
 
 
 $(function() {
@@ -13,5 +15,26 @@ $(function() {
     page: '/',
     title: 'Home'
   })
+
+  var parser = new UAParser(),
+    result = parser.getResult(),
+    os = result.os.name,
+    linkBase = 'http://maps.google.com?q=';
+
+  if(os === 'iOS') {
+    linkBase = 'maps:q=';
+  } else if(os === 'Android') {
+    linkBase = 'geo:';
+  } else if(os === 'Windows Phone' || os === 'Windows Mobile') {
+    linkBase = 'maps:';
+  }
+
+  var linkFull = linkBase + '701%20Ocean%20St,%20Santa%20Cruz,%20CA';
+
+  if(os=='RIM Tablet OS' || os=='BlackBerry') {
+    linkFull = "javascript:blackberry.launch.newMap({'address':{'address1':'701 Ocean St','city':'Santa Cruz','country':'USA','stateProvince':'CA'}});";
+  }
+
+  $('#map-link').attr('href', linkFull);
 
 })
